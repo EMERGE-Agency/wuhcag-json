e465ed7b01bdc2b570220bb04071ea7bebe61226
Joe Watkins <joe@emergeinteractive.com>
2015-10-11 16:53:15 -0700

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function(grunt) {
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    jsonmin: {
+      dev: {
+        options: {
+          stripWhitespace: true,
+          stripComments: true
+        },
+        files: {
+          "dist/wuhcag.min.json" : "dist/wuhcag.json"
+        }
+      }
+    },
+
+    json_merge: {
+      options: {
+        replacer: null,
+        space: " "
+      },
+      your_target: {
+        files: { 'dist/wuhcag.json': ['tmp/**/*.{json,y{,a}ml}'] },
+      },
+    }, // json_merge
+
+    yaml: {
+      your_target: {
+        options: {
+          ignored: /^_/,
+          space: 4,
+          constructors: {
+            '!include': function (node, yaml) {
+              var data = grunt.file.read(node.value, 'utf-8');
+              return yaml.load(data);
+            }
+          }
+        },
+        files: [
+          {
+            expand: true,
+            cwd: 'src/',
+            src: ['**/*.yml'],
+            dest: 'tmp/'
+          }
+        ]
+      }
+    }
+
+
+  });
+
+  // Linting, mostly to test JSON.
+  grunt.registerTask('yaml_to_json', ['yaml']);
+
+  grunt.registerTask('build_json', ['json_merge']);
+
+  grunt.registerTask('minify_json', ['jsonmin']);
+
+  grunt.registerTask('build',['yaml','json_merge','jsonmin']);
+
+
+};
